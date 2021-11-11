c6d6a8a6b2e0b3dc6919d3db886ecb6e4d931d68
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-11 15:32:45 +0100

    navbar-structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Home from './pages/Home'
 
 const App = () => {
     return (
         <div>
-            <h1>App hello!</h1>
+            <Home/>
         </div>
     )
 }
