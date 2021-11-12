56918c397135248e9dc2e4fdc46d586f1e66d7f0
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-12 13:18:40 +0100

    update_slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar'
 import Announcement from '../components/Announcement/Announcement'
+import Slider from '../components/Slider/Slider'
 
 const Home = () => {
     return (
         <div> 
             <Announcement/>
             <Navbar/>
+            <Slider /> 
         </div>
     )
 }
