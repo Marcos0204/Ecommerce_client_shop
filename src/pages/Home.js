0bfdbce939b99fd69be20401b60601eed5d4bcd1
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-12 08:52:09 +0100

    announcement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar'
+import Announcement from '../components/Announcement/Announcement'
 
 const Home = () => {
     return (
-        <div>
+        <div> 
+            <Announcement/>
             <Navbar/>
         </div>
     )
